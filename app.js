a7b51f004218353bdaae1e62dc096834532947bb
ebbersjathomes <ebbersj@homes.com>
2014-10-07 16:43:45 -0400

    added save shutdown on unexpected error

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 "use strict";
 
 var app			= require("./lib/"),
-	instance	= new app(); 
+	instance	= new app(),
+	log			= require("bristol");
 
 instance.run();
+
+process.on('uncaughtException', function(err) {
+       console.log('Uncaught exception:', err, err.stack);
+       log.error(err, {
+               event: "Uncaught exception.  Triggering safe restart.",
+               eventId: "appName:exception"
+       });
+       app.shutdown();
+});
